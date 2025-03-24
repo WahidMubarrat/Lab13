f56a5eab4cc226b32566d45ceeb6cbda9672f194
WahidMubarrat <wahidmubarrat20@gmail.com>
2025-03-24 20:39:59 +0600

    Shortened methods in RolesAndPermission class

--- src/RolesAndPermissions.java
+++ src/RolesAndPermissions.java
@@ -10,11 +10,9 @@ public class RolesAndPermissions extends User {
     public int isPrivilegedUserOrNot(String username, String password) {
         int isFound = -1;
         for (int i = 0; i < adminUserNameAndPassword.length; i++) {
-            if (username.equals(adminUserNameAndPassword[i][0])) {
-                if (password.equals(adminUserNameAndPassword[i][1])) {
-                    isFound = i;
-                    break;
-                }
+            if (username.equals(adminUserNameAndPassword[i][0]) &&
+                    password.equals(adminUserNameAndPassword[i][1])) {
+                return i;
             }
         }
         return isFound;
@@ -28,12 +26,10 @@ public class RolesAndPermissions extends User {
      */
     public String isPassengerRegistered(String email, String password) {
         String isFound = "0";
-        for (Customer c : Customer.customerCollection) {
-            if (email.equals(c.getEmail())) {
-                if (password.equals(c.getPassword())) {
-                    isFound = "1-" + c.getUserID();
-                    break;
-                }
+        for (Customer customer : Customer.customerCollection) {
+            if (email.equals(customer.getEmail()) &&
+                    password.equals(customer.getPassword())) {
+                return "1-" + customer.getUserID();
             }
         }
         return isFound;
